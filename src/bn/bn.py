576f81891623ed7a7261bab2320bb354b5783ba6
Pijush Biswas <pijubisw@in.ibm.com>
2019-01-14 14:49:34 +0530

    Init Python

---
+++ src/bn/bn.py
@@ -0,0 +1,17 @@
+'''
+Created on 14-Jan-2019
+
+@author: PijushBiswas
+'''
+
+class LangBlock(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ src/run.py
@@ -0,0 +1,6 @@
+import os
+import win_unicode_console
+
+win_unicode_console.enable()
+print("===================BANGLA UNICODE TEST =================")
+print("=================== নগরপালিকার ঘরে ঢুকে যাচ্ছে পোয়াবারো  =================")
